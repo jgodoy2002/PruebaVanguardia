d0f6711efa5d020b9f38c107832f6fee268327bf
jgodoy2002 <j.godoy02@unitec.edu>
2025-08-19 17:17:04 -0600

    Hola, creando .env y endpoints en app.js

--- heladeria/app.js
+++ heladeria/app.js
@@ -8,6 +8,13 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+const db = require('./db'); // Assuming you have a db.js file for database connection
+db.connect();
+db.connect({
+  host:process.env.DB_HOST,
+  user:process.env.DB_USER,
+  password:process.env.DB_PASS
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +28,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/gethola',usersRouter);
+app.use('/get',indexRouter);
+app.use('/post',indexRouter);
+app.use('/put/user',indexRouter);
+app.use('/delete/user',indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- heladeria/routes/index.js
+++ heladeria/routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+import dotenv from 'dotenv';
+dotenv.config();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Juan' });
 });
+
+router.get('/',(req, res)=> {
+  res.send('Hello World');
+  });
+
+router.post('/', (req, res) =>{
+  res.send('Get a Post request');
+})  
+
+router.put('/user', (req, res) =>{
+  res.send('Get a Put request at /user');
+});
+
+router.delete('/user', (req, res) => {
+  res.send('Got a DELETE request at /user')
+});
+
 
 module.exports = router;
